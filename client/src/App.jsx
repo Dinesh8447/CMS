1c61f5a2d196e1ad571a2dbb3a3b3bec6917707e
DINESH <93979305+Dinesh8447@users.noreply.github.com>
2023-11-20 19:54:07 +0530

    Update App.jsx
    add register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import About from './compoents/About/About'
 import ProfileCard from './compoents/ProfileCard'
 import Addstudent from './compoents/addstudent/Addstudent'
 import Loginpage from './compoents/loginpage/Login'
-
+import Register from './reference/register'
 import UpdateStudent from './compoents/UpdateStudent/UpdateStudent'
 import ShowAllData_Student from './compoents/StudentPage/ShowAllData_Student'
 import ProfileCard_Student from './compoents/StudentPage/ProfileCard_Student'
@@ -39,7 +39,7 @@ function App() {
     <Route path='/profilecard/:id' element={<ProfileCard/>}/>
     <Route path='/addstudent' element={<Addstudent/>} />
     <Route path='/profilecard/student/:id' element={<ProfileCard_Student/>}/>
-
+    <Route path='/register' element={<Register />} />
     {/* //refer */}
     <Route path='/ref' element={<Ref/>}/>
 
