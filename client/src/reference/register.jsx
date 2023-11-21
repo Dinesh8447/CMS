b13631229f483e8a60b4c73b64318f12c0f8c753
DINESH <93979305+Dinesh8447@users.noreply.github.com>
2023-11-21 08:53:22 +0530

    Update register.jsx
    import link

--- client/src/reference/register.jsx
+++ client/src/reference/register.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+
 
 export default function Register() {
     const[username,setusername] = useState('')
